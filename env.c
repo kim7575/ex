9686d1a90e59134bc01fdccfd6d8b971aa036c15
kim7575 <hounddog@naver.com>
2018-04-27 17:12:08 +0900

    테스트 예제들
    환경변수 , 실행시 매개변수 입력 관련 예제입니다.

---
+++ env.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+int main()
+{
+	setenv("MYDIR", "/home/pi", 0);
+	setenv("KERNEL_SRC", "/usr/src/linux", 0);
+
+	printf("MYDIR = %s\n", getenv("MYDIR"));
+	printf("KERNEL_SRC = %s\n", getenv("KERNEL_SRC"));
+
+	// 현재 설정된 환경 변수 값을 모두 출력할 경우
+	
+	   while(*environ)
+		printf("%s\n", *environ++);
+	
+
+	return 0;
+}
